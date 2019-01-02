d24e00532752962238522150117ee804c6722183
kyori19 <kyori.dis@gmail.com>
2019-01-02 11:44:21 +0900

    Configure copyright

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,3 +34,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+COPYRIGHT_NAME = "Team AccelForce"
+COPYRIGHT_YEAR = 2019
